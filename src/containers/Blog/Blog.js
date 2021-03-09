27bf83be4ddac52dbb8529cb0618a789843cdd9b
Sary Jamie <sary.jamie@gmail.com>
2021-03-09 01:51:58 -0800

    adding a selectedPostId state to the class to make sure we are selecting the post and then pass a function postSlectedHandler to make an action once the post is clicked and display it to the FullPost component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,6 +9,7 @@ import "./Blog.css";
 class Blog extends Component {
   state = {
     posts: [],
+    selectedPostId: null,
   };
 
   componentDidMount() {
@@ -24,15 +25,27 @@ class Blog extends Component {
       //   console.log(response);
     });
   }
+
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
   render() {
     const posts = this.state.posts.map((post) => {
-      return <Post key={post.id} title={post.title} author={post.author} />;
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
     });
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
