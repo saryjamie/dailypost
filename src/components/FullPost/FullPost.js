61ba8e3a36d178720dcf17e486fc119a45d58ce1
Sary Jamie <sary.jamie@gmail.com>
2021-03-12 13:04:39 -0800

    fixing the placeholder url so that we dont have infinite loop in the network tap

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -14,7 +14,7 @@ class FullPost extends Component {
         (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
       ) {
         axios
-          .get("http://jsonplaceholder.typicode.com/posts/1" + this.props.id)
+          .get("http://jsonplaceholder.typicode.com/posts/" + this.props.id)
           .then((response) => {
             //   console.log(response);
             this.setState({ loadedPost: response.data });
