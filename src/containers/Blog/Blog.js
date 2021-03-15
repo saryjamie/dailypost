9e0a6d33b98f5fdf4fe3021de4e943cd1b88396d
Sary Jamie <sary.jamie@gmail.com>
2021-03-14 23:36:12 -0700

    hardcoding

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -21,7 +21,7 @@ class Blog extends Component {
         const updatedPosts = posts.map((post) => {
           return {
             ...post,
-            author: "Sary",
+            author: "Dana",
           };
         });
         this.setState({ posts: updatedPosts });
