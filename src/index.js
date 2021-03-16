de5ec34db4776ae943a55bc1049a6edc23a71ba6
Sary Jamie <sary.jamie@gmail.com>
2021-03-16 02:08:30 -0700

    added a second function to the request configuration changing function that handles any errors

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import axios from "axios";
 
-axios.interceptors.request.use((request) => {
-  console.log(request);
-  return request;
-});
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    return request;
+  },
+  (error) => {
+    console.log(error);
+  }
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
