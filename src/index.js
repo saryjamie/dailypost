8a848bbb8b922954e2af005a92fb259d87801413
Sary Jamie <sary.jamie@gmail.com>
2021-03-17 05:14:44 -0700

    adding axios interceptors for a request and then for a response in the index.js file so that we can handle the error locally and globally as well

--- src/index.js
+++ src/index.js
@@ -16,5 +16,16 @@ axios.interceptors.request.use(
   }
 );
 
+axios.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
