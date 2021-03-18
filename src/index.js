7575952589f7cd82ab04273852683c6dfdbc9b92
Sary Jamie <sary.jamie@gmail.com>
2021-03-18 01:28:15 -0700

    adding axios.defaults.headers.common and post to the index.js file

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import registerServiceWorker from "./registerServiceWorker";
 import axios from "axios";
 
 axios.defaults.baseURL = "http://jsonplaceholder.typicode.com";
+axios.defaults.headers.common["Authoriaztion"] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 axios.interceptors.request.use(
   (request) => {
