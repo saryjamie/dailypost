33b084f680b196186c30b8da50cf82a6f5a5d0e6
Sary Jamie <sary.jamie@gmail.com>
2021-03-18 22:44:59 -0700

    uncomment the axios baseURL in index.js to get the best of both worlds

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,9 @@
-import React from "react";
 import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://jsonplaceholder.typicode.com",
 });
 
-instance.axios.defaults.headers.common["Authoriaztion"] =
-  "AUTH TOKEN FROM INSTANCE";
+instance.defaults.headers.common["Authoriaztion"] = "AUTH TOKEN FROM INSTANCE";
 
 export default instance;
