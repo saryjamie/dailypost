1b099b679e56638d6121a068e0a7c9fb2de07ab0
Sary Jamie <sary.jamie@gmail.com>
2021-04-07 15:26:14 -0700

    returning posts section in the class component Posts

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
-class Posts extends Component {}
-
-<section className="Posts">{posts}</section>;
+class Posts extends Component {
+  render() {
+    return <section className="Posts">{posts}</section>;
+  }
+}
