9176f115d36af7f7076cabd3eb85334705fc1de2
Sary Jamie <sary.jamie@gmail.com>
2021-04-14 18:34:37 -0700

    commenting out R<Route /> and replace with <Route path exact component and fetch the Posts component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 // import axios from "../../axios";
 import { Route } from "react-router-dom";
 import "./Blog.css";
+import Posts from "./Posts/Posts";
 // import Posts from "./Posts/Posts";
 
 class Blog extends Component {
@@ -20,7 +21,8 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Route path="/" exact render={() => <h1>Home</h1>} />
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
+        <Route path="/" exact component={Posts} />
       </div>
     );
   }
