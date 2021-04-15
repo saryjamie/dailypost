cd294ac6630e1e52a2b760d45679eeb375a58393
Sary Jamie <sary.jamie@gmail.com>
2021-04-15 12:37:49 -0700

    importing Link from the react-router-dom package to use Link tags instead if the anchor tag so that we can reload the component instead of loading the whole page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 // import axios from "../../axios";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import "./Blog.css";
 import Posts from "./Posts/Posts";
 // import Posts from "./Posts/Posts";
