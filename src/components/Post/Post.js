2e0ef2ef9cc06db0cec381395f8b7e0bebc98911
Sary Jamie <sary.jamie@gmail.com>
2021-04-18 15:37:45 -0700

    importing withRouter from rect-router-dom package in the Post.js file and wrap the post export by withRouter

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { withRouter } from "react-router-dom";
 import "./Post.css";
 
 const post = (props) => {
@@ -14,4 +14,4 @@ const post = (props) => {
   );
 };
 
-export default post;
+export default withRouter(post);
