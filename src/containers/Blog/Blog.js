dadeb2e608da37f2a34468addfb72e5c9048459b
Sary Jamie <sary.jamie@gmail.com>
2021-04-19 05:20:13 -0700

    cleaning code in Blog.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
-// import axios from "../../axios";
+
 import { Route, Link } from "react-router-dom";
 import "./Blog.css";
 import Posts from "./Posts/Posts";
-// import Posts from "./Posts/Posts";
 
 import NewPost from "./NewPost/NewPost";
 
